527b9abcb682508162aae4b444ba8adb9c66af27
Daisuke Nakahara <daisuken@msn.com>
2020-03-24 04:14:28 -0700

    Event Hub Producer and Consumer Sample App

---
+++ EventHubs/Consumer/Consumer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Consumer
+{
+    class Consumer
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.FullName = "Event Hubs Consumer";
+            app.Description = "Event Hubs Consumer Sample";
+            app.HelpOption("-h|-?|--help");
+
+            var connectionString = app.Option("-cs", "Specify ConnectionString", CommandOptionType.SingleValue, true);
+            var group            = app.Option("-cg", "Specify Consumer Group", CommandOptionType.SingleValue);
+            var hub              = app.Option("-hub", "Specify Event Hub", CommandOptionType.SingleValue);
+            var allEvents        = app.Option("-all", "Read all events", CommandOptionType.NoValue);
+            app.Execute(args);
+
+            if (!connectionString.HasValue() || string.IsNullOrEmpty(connectionString.Value()))
+            {
+                ShowHelp();
+                return 0;
+            }
+
+            if (!hub.HasValue() || string.IsNullOrEmpty(hub.Value()))
+            {
+                ShowHelp();
+                return 0;
+            }
+
+            //
+            // https://docs.microsoft.com/en-us/dotnet/api/azure.messaging.eventhubs.consumer.eventhubconsumerclient?view=azure-dotnet
+            // Select Consumer Group
+            //
+            string consumerGroup;
+
+            if (group.HasValue() && !string.IsNullOrEmpty(group.Value()))
+            {
+                consumerGroup = group.Value();
+            }
+            else
+            {
+                consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+            }
+
+            Console.WriteLine("Connection String : {0}....", connectionString.Value().Substring(0, 50));
+            Console.WriteLine($"Event Hub         : {hub.Value()}");
+            Console.WriteLine($"Consumer Group    : {consumerGroup}");
+            Console.WriteLine($"Read all events   : {allEvents.HasValue()}");
+
+            EventHubConsumerClient consumer = new EventHubConsumerClient(consumerGroup, connectionString.Value(), hub.Value());
+
+            var part = await consumer.GetPartitionIdsAsync();
+            var part_prop = await consumer.GetPartitionPropertiesAsync(part[0]);
+            var hub_prop = await consumer.GetEventHubPropertiesAsync();
+
+            using CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            cancellationSource.CancelAfter(TimeSpan.FromSeconds(90));
+
+            //
+            // https://docs.microsoft.com/en-us/dotnet/api/azure.messaging.eventhubs.consumer.eventhubconsumerclient.readeventsasync?view=azure-dotnet
+            //
+            // Timeout 10 Min
+            //
+            ReadEventOptions readOptions = new ReadEventOptions
+            {
+                MaximumWaitTime = TimeSpan.FromSeconds(600)
+            };
+
+            try
+            {
+                Console.WriteLine($"Read Start        : {DateTime.Now.ToString()}");
+                await foreach (PartitionEvent ev in consumer.ReadEventsAsync(startReadingAtEarliestEvent: allEvents.HasValue(), readOptions, cancellationSource.Token))
+                {
+                    Console.WriteLine("Enqueu at {0:yyyy/MM/dd H:mm:ss:fff} | Seq # {1:D4} | Partition {2} | Offset : {3:D6} | Data {4}",
+                        ev.Data.EnqueuedTime,
+                        ev.Data.SequenceNumber,
+                        ev.Partition.PartitionId,
+                        ev.Data.Offset,
+                        Encoding.UTF8.GetString(ev.Data.Body.ToArray())
+                        );
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception         : {ex.Message}");
+            }
+            Console.WriteLine($"Read Exit         : {DateTime.Now.ToString()}");
+
+            return 1;
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Please specify Event Hub Connection string");
+            Console.WriteLine("dotnet run consumer -cs \"<Connection String>\" -cg <Consumer Group> -hub <Event Hub Name>");
+            Console.WriteLine("  -all to read all events.  If not specified, read new events only");
+        }
+    }
+}
---
+++ EventHubs/Producer/Producer.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Procucer
+{
+    class Producer
+    {
+        private const int numEvents = 12;
+
+        static async Task<int> Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.FullName = "Event Hubs Producer";
+            app.Description = "Event Hubs Consumer Sample";
+            app.HelpOption("-h|-?|--help");
+
+            var connectionString = app.Option("-cs", "Specify Connection String", CommandOptionType.SingleValue);
+            var hub              = app.Option("-hub", "Specify Consumer Group", CommandOptionType.SingleValue);
+
+            app.Execute(args);
+
+            if (!connectionString.HasValue() || string.IsNullOrEmpty(connectionString.Value()))
+            {
+                ShowHelp();
+                return 0;
+            }
+
+            if (!hub.HasValue() || string.IsNullOrEmpty(hub.Value()))
+            {
+                ShowHelp();
+                return 0;
+            }
+
+            Console.WriteLine("Connection String : {0}....", connectionString.Value().Substring(0,50));
+            Console.WriteLine($"Event Hub         : {hub.Value()}");
+
+            //
+            // https://docs.microsoft.com/en-us/dotnet/api/azure.messaging.eventhubs.producer.eventhubproducerclient?view=azure-dotnet
+            //
+            await using (EventHubProducerClient ehProducer = new EventHubProducerClient(connectionString.Value(), hub.Value()))
+            {
+                try
+                {
+                    Console.WriteLine("--------------------------------------------------------");
+                    Console.WriteLine($"Sending {numEvents} individual events");
+
+                    for (var i = 0; i < numEvents; i++)
+                    {
+                        using (EventDataBatch eventData = await ehProducer.CreateBatchAsync())
+                        {
+                            var message = string.Format("Message # {0:D2}", i);
+                            eventData.TryAdd(new EventData(Encoding.UTF8.GetBytes(message)));
+                            await ehProducer.SendAsync(eventData);
+                        }
+                    }
+
+                    Console.WriteLine("--------------------------------------------------------");
+                    Console.WriteLine($"Sending {numEvents} events in a batch");
+                    Console.WriteLine($"Press Enter to continue");
+                    char ch = Console.ReadKey(true).KeyChar;
+
+                    using (EventDataBatch eventData = await ehProducer.CreateBatchAsync())
+                    { 
+                        for (var i = 0; i < numEvents; i++)
+                        {
+                            var message = string.Format("Message # {0:D2} Batched", i);
+                            eventData.TryAdd(new EventData(Encoding.UTF8.GetBytes(message)));
+                        }
+                        await ehProducer.SendAsync(eventData);
+                    }
+
+                    Console.WriteLine("--------------------------------------------------------");
+                    Console.WriteLine($"Sending {numEvents} individual events with Partition Key");
+                    Console.WriteLine($"Press Enter to continue");
+                    ch = Console.ReadKey(true).KeyChar;
+
+                    CreateBatchOptions batchOption = new CreateBatchOptions {PartitionKey = "MyPartitionKey" };
+
+                    for (var i = 0; i < numEvents; i++)
+                    {
+                        using (EventDataBatch eventData = await ehProducer.CreateBatchAsync(batchOption))
+                        {
+                            var message = string.Format("Message # {0:D2} with Partition Key", i);
+                            eventData.TryAdd(new EventData(Encoding.UTF8.GetBytes(message)));
+                            await ehProducer.SendAsync(eventData);
+                        }
+                    }
+
+                    Console.WriteLine("--------------------------------------------------------");
+                    var partitionIds = await ehProducer.GetPartitionIdsAsync();
+                    Console.WriteLine($"Sending {partitionIds.Length} events to each partition");
+                    Console.WriteLine($"Press Enter to continue");
+                    ch = Console.ReadKey(true).KeyChar;
+
+
+                    foreach(var partitionId in partitionIds)
+                    {
+                        batchOption = new CreateBatchOptions { PartitionId = partitionId };
+                        for (var i = 0; i < partitionIds.Length; i++)
+                        {
+                            using (EventDataBatch eventData = await ehProducer.CreateBatchAsync(batchOption))
+                            {
+                                var message = string.Format("Message # {0:D2} to Partition {1:D2}", i, partitionId);
+                                eventData.TryAdd(new EventData(Encoding.UTF8.GetBytes(message)));
+                                await ehProducer.SendAsync(eventData);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{Environment.NewLine}Exception: {ex.Message}");
+                }
+                await ehProducer.CloseAsync();
+            }
+
+            return 1;
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Please specify Event Hub Connection string");
+            Console.WriteLine("dotnet run producer -cs \"<Connection String>\"");
+        }
+    }
+}
